215620099e2be205ef75fd2f652b33ab2217c032
GFLJS2100-user <reyedtreefrog@gmail.com>
2024-11-11 22:02:55 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# bytebeat-composer
+Live editing algorithmic music generator with a collection of many formulas from around the internet.
+![Serpinsky](https://user-images.githubusercontent.com/105890603/229014766-38a7c067-55d3-4120-9ed8-2a8aeb4c1f20.png)
---
+++ main.js
@@ -0,0 +1,8 @@
+// register ServiceWorker, remember to use absolute path!
+// if (navigator.serviceWorker) {
+//   navigator.serviceWorker.register('/gfljs2100-user.github.io/gfljbeat2/sw.js', {scope: '/gfljs2100-user.github.io/gfljbeat2/'})
+// }
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/gfljbeat2/sw.js'
+}
